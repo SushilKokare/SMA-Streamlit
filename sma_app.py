b2aac940360ecf94e5fab10558dd984dd8e1b05c
Sushil Kokare <54114910+SushilKokare@users.noreply.github.com>
2024-06-06 22:17:27 +0530

    added theme option sma_app.py

--- sma_app.py
+++ sma_app.py
@@ -32,6 +32,23 @@ st.markdown(light, unsafe_allow_html=True)
 
 # Create a toggle button
 toggle = st.button("Toggle theme")
+# Use a global variable to store the current theme
+if "theme" not in st.session_state:
+    st.session_state.theme = "light"
+
+# Change the theme based on the button state
+if toggle:
+    if st.session_state.theme == "light":
+        st.session_state.theme = "dark"
+    else:
+        st.session_state.theme = "light"
+
+# Apply the theme to the app
+if st.session_state.theme == "dark":
+    st.markdown(dark, unsafe_allow_html=True)
+else:
+    st.markdown(light, unsafe_allow_html=True)
+
 
 def get_data(symbol,start=None,end=None,period='max'):
     df = pd.DataFrame()
